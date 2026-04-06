94c559e625330915d949f2144ae99e14fe411a2f
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-04-06 08:54:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hello i'm Mohammad Thoufeeq")
+let studies="BE 2nd year"
+console.log(studies)
+let year=3
+console.log(year)
+
+//Templet literals(used to add the embaded expressions)
+let ST_1_marks= 9;
+let ST_2_marks=8;
+let ST_3_marks=9;
+let total_avg_marks=(ST_1_marks+ST_2_marks+ST_3_marks)/3;
+console.log(`The average of the st is ${total_avg_marks}`)

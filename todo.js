20482d5801efe70c8806c020341793c404b96f72
mohammadthoufeeq9 <thoufeeqa161@gmail.com>
2026-04-12 01:05:44 +0530

    todo

--- todo.js
+++ todo.js
@@ -7,13 +7,20 @@ while (true){
     }
     if(req=='list'){
         console.log("-----------------");
-        for(task of todo){
-            console.log(task);
+        for(let i=0; i<todo.length; i++){
+            console.log(i,todo[i]);
         }
         console.log("-----------------");
     }else if(req=="add"){
         let task=prompt("enter the task that you want to enter");
         todo.push(task);
         console.log("Added to todo")
+    }else if (req=="delete"){
+        let idx=prompt("Enter the index that you want to remove");
+        todo.splice(idx,1);
+        console.log("task deleted from todo list")
+    }else{
+        console.log("Wrong request");
     }
+    req=prompt("Please enter your request:");
 }
